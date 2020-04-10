597ce1b607aa5930f7190055217fab2ec4a0adaa
ghorab moundir razine <razinemg@gmail.com>
2020-04-10 21:52:08 +0100

    second commit

--- sat-instances.py
+++ sat-instances.py
@@ -2,16 +2,24 @@ satFile=open("UF75.325.100/uf75-01.cnf")
 instances=[]
 instance=[]
 lineCounter=0
-
-while satFile.readline() !='':
-     x=satFile.readline()
+fileEndingBool=True
+while fileEndingBool:
+     
+     x=satFile.readline() 
+     
+     if x[0] == "%":
+         fileEndingBool=False
      if x[0] != "c" and x[0] !="p" and x[0] !="%" and x[0] !="0" :
          y=x.replace('0\n','').replace("\n","")
          c=y.split(" ")
-         print(c)
-    
+         if c[0] == '' :
+             c.pop(0)
+         c.pop(3)
+         instance.append(list(map(int,c)))
+         lineCounter+=1
+     
  
-
-   
-
+print(lineCounter)
+print(instance)  
+print(instance[2][0])
     
