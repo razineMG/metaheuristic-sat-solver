a54f8e0496192320f94e777a81ae2e3b40cfa0d6
ghorab moundir razine <razinemg@gmail.com>
2020-04-10 22:59:35 +0100

    third commit

--- sat-instances.py
+++ sat-instances.py
@@ -1,25 +1,32 @@
-satFile=open("UF75.325.100/uf75-01.cnf")
+
 instances=[]
 instance=[]
-lineCounter=0
-fileEndingBool=True
-while fileEndingBool:
+sizeOfInstances=100
+filePath=1
+i=0
+for i in range(sizeOfInstances) :
+    
+    satFile=open("UF75.325.100/uf75-0"+str(filePath)+".cnf")
+    lineCounter=0
+    fileEndingBool=True
+    while fileEndingBool:
      
-     x=satFile.readline() 
+         x=satFile.readline() 
      
-     if x[0] == "%":
-         fileEndingBool=False
-     if x[0] != "c" and x[0] !="p" and x[0] !="%" and x[0] !="0" :
-         y=x.replace('0\n','').replace("\n","")
-         c=y.split(" ")
-         if c[0] == '' :
-             c.pop(0)
-         c.pop(3)
-         instance.append(list(map(int,c)))
-         lineCounter+=1
+         if x[0] == "%":
+             fileEndingBool=False
+         if x[0] != "c" and x[0] !="p" and x[0] !="%" and x[0] !="0" :
+             y=x.replace('0\n','').replace("\n","")
+             c=y.split(" ")
+             if c[0] == '' :
+                 c.pop(0)
+             c.pop(3)
+             instance.append(list(map(int,c)))
+             lineCounter+=1
+    filePath+=1         
      
  
 print(lineCounter)
 print(instance)  
-print(instance[2][0])
+
     
